00a31f0067d84a7d870e8d3027e1b8843f641b85
Max Gaponov <gaponovmaxev@gmail.com>
2021-02-28 06:36:05 +0300

    updated query fields

--- application/router.py
+++ application/router.py
@@ -5,23 +5,17 @@ from application.models import Word, User
 
 @app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
-    try:
-        id = request.form['id']
-        user = User.query.filter_by(sber_id=id).first()
-        if user is None:
-            db.session.add(User(id, 0))
-            db.session.commit()
-            return json.dumps({'level': 0})
-        else:
-            return json.dumps({'level': user.level})
-    except KeyError:
+    id = request.values['id']
+    user = User.query.filter_by(sber_id=id).first()
+    if user is None:
+        db.session.add(User(id, 0))
+        db.session.commit()
         return json.dumps({'level': 0})
+    else:
+        return json.dumps({'level': user.level})
 
 @app.route('/next_word', methods=['GET', 'POST'])
 def next_word():
-    try:
-        id = request.form['id']
-        wa = request.form['wa']
-        return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
-    except KeyError:
-        return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
+    id = request.values['id']
+    wa = request.values['wa']
+    return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
