dea53b53823bbe69303634194cb79287d0cae387
Max Gaponov <gaponovmaxev@gmail.com>
2021-02-28 05:07:35 +0300

    _

--- application/router.py
+++ application/router.py
@@ -5,11 +5,17 @@ from application.models import Word, User
 
 @app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
-    id = request.form['id']
-    return json.dumps({'level': 0})
+    try:
+        id = request.form['id']
+        return json.dumps({'level': 0})
+    except KeyError:
+        return json.dumps({'level': 0})
 
 @app.route('/next_word', methods=['GET', 'POST'])
 def next_word():
-    id = request.form['id']
-    wa = request.form['wa']
-    return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
+    try:
+        id = request.form['id']
+        wa = request.form['wa']
+        return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
+    except KeyError:
+        return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
