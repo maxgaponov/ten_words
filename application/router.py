a996adf9962c366e007b51e67fc81a6743c793d3
Max Gaponov <gaponovmaxev@gmail.com>
2021-02-28 06:58:12 +0300

    json response

--- application/router.py
+++ application/router.py
@@ -8,16 +8,29 @@ def new_game():
     req_data = request.get_json()
     id = req_data['id']
     user = User.query.filter_by(sber_id=id).first()
+    res = {}
     if user is None:
         db.session.add(User(id, 0))
         db.session.commit()
-        return json.dumps({'level': 0})
+        res = {'level': 0}
     else:
-        return json.dumps({'level': user.level})
+        res = {'level': user.level}
+    response = app.response_class(
+        response=json.dumps(res),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/next_word', methods=['GET', 'POST'])
 def next_word():
     req_data = request.get_json()
     id = req_data['id']
     wa = req_data['wa']
-    return json.dumps({'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False})
+    res = {'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False}
+    response = app.response_class(
+        response=json.dumps(res),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
