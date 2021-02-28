21b8044307094771280ea99f4731eb6632bc9a87
Max Gaponov <gaponovmaxev@gmail.com>
2021-02-28 07:30:57 +0300

    added words learning

--- application/router.py
+++ application/router.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from flask import request
 from application import app, db, user_data
 from application.models import Word, User
@@ -7,6 +8,7 @@ from application.models import Word, User
 def new_game():
     req_data = request.get_json()
     id = req_data['id']
+    
     user = User.query.filter_by(sber_id=id).first()
     res = {}
     if user is None:
@@ -15,6 +17,13 @@ def new_game():
         res = {'level': 0}
     else:
         res = {'level': user.level}
+
+    user_data[id] = {
+        'word_list': list(map(lambda word: (word.rus, word.eng), Word.query.filter_by(level=res['level'], difficulty=2).all())),
+        'cur_word': 0,
+        'testing_phase': False,
+    }
+
     response = app.response_class(
         response=json.dumps(res),
         status=200,
@@ -27,7 +36,17 @@ def next_word():
     req_data = request.get_json()
     id = req_data['id']
     wa = req_data['wa']
-    res = {'word_rus': 'Собака', 'word_eng': 'Dog', 'end': False}
+
+    res = {}
+
+    if user_data[id]['testing_phase']:
+        pass
+    else:
+        idx = user_data[id]['cur_word']
+        cur_word = user_data[id]['word_list'][idx]
+        res = {'word_rus': cur_word[0], 'word_eng': cur_word[1], 'end': False}
+        user_data[id]['cur_word'] += 1
+    
     response = app.response_class(
         response=json.dumps(res),
         status=200,
